395d5b69fd3fcb5aade23c167c0d60d2bf1f3800
alenamartinkova <martinkova.a@gmail.com>
2021-12-13 18:09:50 +0100

    Final edits

--- main.py
+++ main.py
@@ -21,17 +21,15 @@ def cykParse(word):
     word = list(word)
     n = len(word)
 
-    # Initialize the table
     T = [[dict() for j in range(n)] for i in range(n)]
 
-    # Filling in the table
     for i in range(n):
 
-        # Iterate over the rules
+        # Prechádzam pravidlá
         for l, rule in R.items():
             for r in rule:
 
-                # If a terminal is found
+                # Ak nájdem terminál
                 if len(r) == 1 and r[0] == w[i]:
                     T[0][i][l] = 1
 
@@ -40,14 +38,14 @@ def cykParse(word):
 
         for j in range(n - 1 - i):
             for comb in combinations:
-                # comb represents one possible divide of word - e.g. n = 3, then it can be comb[0] = 2, comb[1] = 1
-                # so we know that we are looking for combination of subword of length 2 and subword of length 1
-                # subword of length 2 is in second row (indexing from 0, so on index 1, that's why - 1)
-                # subword of length 1 is in first row (indexing from 0, so on index 0, that's why - 1) and
-                # + comb[0] because the word will continue in column of that index
+                # comb reprezentuje jedno možné rozdelenie slova - napr. n = 3, tak comb[0] = 2, comb[1] = 1
+                # takže viem že hľadám kombináciu podslov dĺžky 2 a 1
+                # podslovo dĺžky 2 je v druhom riadku (indexujem od 0, takže index 1, preto - 1)
+                # podslovo dĺžky 1 je v prvom riadku (indexujem od 0, takže 0,  preto - 1) a
+                # + comb[0], pretože slovo začne v stĺpci tohto indexu
                 for x in T[comb[0] - 1][j]:
                     for y in T[comb[1] - 1][j + comb[0]]:
-                        # Iterate over the rules
+                        # Prechádzam pravidlá
                         for l, rule in R.items():
                             for r in rule:
                                 if len(r) == 2 and r[0] == x and r[1] == y:
@@ -58,11 +56,11 @@ def cykParse(word):
         print(T[i])
 
 
-# Non-terminal & terminal symbols
+# Symboly
 non_terminals = ["S", "A", "B", "C"]
 terminals = ["a", "b"]
 
-# Rules of the grammar
+# Pravidlá
 R = {
     "S": [["A", "B"], ["B", "C"]],
     "A": [["B", "A"], ["a"]],
@@ -70,8 +68,8 @@ R = {
     "C": [["A", "B"], ["a"]]
 }
 
-# Given string
+# Slovo
 w = "baaba"
 
-# Function Call
+# Volanie algoritmu
 cykParse(w)
